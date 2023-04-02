42d140fcd8b430f8da8c318d0400781c5e11714b
FEEL1CE <fardannurhidayat12@gmail.com>
2023-04-02 19:23:23 +0700

    Nanana

--- main.php
+++ main.php
@@ -23,11 +23,35 @@
     return $this->color;
   }
  }
+
+  
+ class mahasiswa
+ {
+  public $nama;
+  private $NIMl;
+  public $jurusan;
+
+  function set_ngaran($nama,$NIM, $jurusan)
+  {
+    $this->nama = $nama;
+    $this->NIM = $NIM;
+    $this->jurusan=$jurusan;
+  }
+  function get_ngaran()
+  {
+    return $this->jurusan.  $this->nama .$this->NIM;
+  }
+ }
+
+
  
  $manggo = new fruit;
  $manggo->set_name("Manggo ");
  $manggo->set_color("Green");
 
+$pria = new mahasiswa;
+$pria->set_ngaran("fardan", " 220102007 " , "TI ");
+echo $pria->get_ngaran();
 
  echo $manggo->get_name();
  echo $manggo->get_color();
